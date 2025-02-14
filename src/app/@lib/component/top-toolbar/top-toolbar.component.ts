2edbb1aa2dd4ff982b345335675c2a18303eef3f
YC <youyongchean@gmail.com>
2025-02-14 08:46:18 +0800

    #develop: add tailwind css v3

---
+++ src/app/@lib/component/top-toolbar/top-toolbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-toolbar',
+  imports: [],
+  templateUrl: './top-toolbar.component.html',
+  styleUrl: './top-toolbar.component.scss',
+})
+export class TopToolbarComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopToolbarComponent } from '@lib/component/top-toolbar/top-toolbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopToolbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
