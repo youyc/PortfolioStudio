961cd4f91e48b42e5755eb903ab4bf8e47c64722
YC <youyongchean@gmail.com>
2025-02-16 11:02:37 +0800

    #develop: add intro and working experience scroll section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AppTopToolbarComponent } from './@config/component/app-top-toolbar/app-top-toolbar.component';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppTopToolbarComponent, ButtonModule],
+  imports: [RouterOutlet, AppTopToolbarComponent, ButtonModule, TabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
