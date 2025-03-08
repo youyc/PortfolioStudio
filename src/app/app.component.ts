4ab68152a3758e1fced286f2229a674ff0306d1a
YC <youyongchean@gmail.com>
2025-03-09 04:39:34 +0800

    #developV2: add projects page with gsap animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AppTopToolbarComponent } from './@config/component/app-top-toolbar/app-top-toolbar.component';
@@ -15,7 +15,25 @@ import { TextPlugin } from 'gsap/all';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit {
+  constructor(private _ngZone: NgZone) {}
+
   title = 'portfolio';
+  projectList: any[] = [
+    {
+      Name: 'Landing Page',
+      Info: 'Responsive Web',
+      Image: 'img',
+      GSAP: null,
+    },
+    {
+      Name: 'Inventory App',
+      Info: 'Operation Mobile App',
+      Image: 'img',
+      GSAP: null,
+    },
+  ];
+
+  projectItemAnimation: any;
 
   // const bgGroup=document.querySelector(".bg-group");
   // const bgPurple=document.querySelector(".bg-purple");
@@ -41,35 +59,65 @@ export class AppComponent implements AfterViewInit {
   // })
 
   ngAfterViewInit(): void {
-    gsap.registerPlugin(ScrollTrigger, TextPlugin);
-    // gsap.to('.box', { duration: 1, x: 100, rotation: 360 });
-    const tltest = gsap.timeline();
+    this._ngZone.runOutsideAngular(() => {
+      gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
-    // //鏈式寫法
-    // tltest
-    //   .from('.section1-title', { xPercent: -100 })
-    //   .from('section1-subtitle', { xPercent: 100 })
-    //   .from('.section1-description', { yPercent: -100 });
+      // Title Animation
+      gsap.fromTo(
+        '.section1-title',
+        { scale: 0, opacity: 0 },
+        {
+          duration: 1,
+          scale: 1.0,
+          opacity: 1,
+          text: { value: 'Digital Slime Studio' },
+        }
+      );
 
-    // ScrollTrigger.create({
-    //   animation: tltest,
-    //   start: 'top top',
-    //   // end: '+=4000',
-    //   scrub: true,
-    //   pin: '.box',
-    //   markers: true,
-    // });
+      // Project Animation
+      this.projectList.forEach((item, index) => {
+        item.GSAP = gsap
+          .to('.project-bg-' + index, {
+            duration: 0.5,
+            width: '100%',
+            // background: 'blue',
+            ease: 'circ.in',
+          })
+          .reverse();
+        // .to('.project-item-' + index, {
+        //   duration: 1,
+        //   // backgroundSize: '100% 100%',
+        //   // enableBackground: 1,
+        //   background: 'blue',
+        //   // backgroundImage: 'url(pixel-slime-blue-removebg.png)',
+        //   // backgroundImage: 'pixel-slime-blue-removebg.png', //pixel-slime-blue-removebg.png
+        // })
+        // .reverse();
+      });
 
-    gsap.fromTo(
-      '.section1-title',
-      { scale: 0, opacity: 0 },
-      {
-        duration: 1,
-        y: 0,
-        scale: 1.0,
-        opacity: 1,
-        text: { value: 'Digital Slime Studio' },
-      }
-    );
+      // const hoverTL = gsap.timeline();
+      // this.projectItemAnimation = hoverTL
+      //   .to('.project-item', {
+      //     duration: 1,
+      //     background: 'blue',
+      //   })
+      //   .reverse();
+
+      // FOR SAME CLASS
+      // var sections = gsap.utils.toArray('.project-item');
+      // sections.forEach((section: any) => {
+      //   gsap.to(section, {
+      //     duration: 1,
+      //     background: 'blue',
+      //   });
+      // });
+    });
+  }
+
+  mouseEnter(index: number) {
+    this.projectList[index].GSAP.play();
+  }
+  mouseLeave(index: number) {
+    this.projectList[index].GSAP.reverse();
   }
 }
